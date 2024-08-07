4b34ebbd227d6d78fcfe606521d896b3de4a6f19
simunde <sidmundece17@gmail.com>
2024-08-07 10:36:08 -0400

    created test cases to navigate to all fragments

--- app/src/androidTest/java/com/msid/bottomnavigationapp/MainActivityTest.kt
+++ app/src/androidTest/java/com/msid/bottomnavigationapp/MainActivityTest.kt
@@ -18,8 +18,20 @@ class MainActivityTest{
 
     @Test
     fun test_navigation(){
-
+        // Navigate to DashBoard
         onView(withId(R.id.nav_dashboard)).perform(click())
-        onView(withId(R.id.text_dashboard)).check(matches(isDisplayed()) )
+        onView(withId(R.id.text_dashboard)).check(matches(isDisplayed()))
+
+        // Navigate to Notifications
+        onView(withId(R.id.nav_notifications)).perform(click())
+        onView(withId(R.id.text_notifications)).check(matches(isDisplayed()))
+
+        // Navigate to Profile
+        onView(withId(R.id.nav_profile)).perform(click())
+        onView(withId(R.id.text_profile)).check(matches(isDisplayed()))
+
+        // Navigate to Home
+        onView(withId(R.id.nav_home)).perform(click())
+        onView(withId(R.id.text_home)).check(matches(isDisplayed()))
     }
 }
